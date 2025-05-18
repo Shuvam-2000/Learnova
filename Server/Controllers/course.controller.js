bd1f1d185f388b48735e1b3f13f7c27c4e7ca54d
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-05-18 21:06:01 +0530

    feat: functionality added to create new course

--- Server/Controllers/course.controller.js
+++ Server/Controllers/course.controller.js
@@ -1,8 +1,29 @@
-import CourseModel from "../models/courses.model";
+import CourseModel from "../models/courses.model.js";
 
 // creating new courses
 export const createNewCourse = async (req,res) => {
     try {
+        const { courseName, courseDescription, coursePrice, image } = req.body
+
+        // check if all fields are provided
+        if( !courseName || !courseDescription || !coursePrice ) return res.status(400).json({
+            message: "All Fields Required",
+            success: false
+        })
+
+        // create new course
+        const course = new CourseModel ({
+            courseName,
+            courseDescription,
+            coursePrice,
+            image
+        })
+
+        await course.save()
+        res.status(201).json({
+            message: `${courseName} course created successfully`,
+            success: true
+        })
         
     } catch (error) {
         res.status(500).json({
@@ -11,7 +32,6 @@ export const createNewCourse = async (req,res) => {
         })
     }
 }
-
 
 // updating a existing course
 export const updateExisitngCourse = async (req,res) => {
