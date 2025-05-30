ee7e84221633f0e53643fc1c0fbfb568553c8e4d
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-05-30 16:35:41 +0530

    feat: cookie-parser middleware added, routes added for user and course

--- Server/app.js
+++ Server/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { configDotenv } from 'dotenv';
 import './utils/connection.js'
 import courseRoute from './routes/course.routes.js'
+import userRoute from './routes/user.routes.js'
 
 // initialize the app
 const app = express();
@@ -16,6 +18,7 @@ const PORT = process.env.PORT || 8001;
 // middlewares
 app.use(express.json())  // Parse JSON request
 app.use(express.urlencoded({ extended: true }))  // Parse URL-encoded request bodies
+app.use(cookieParser());
 app.use(cors());
 
 // test route
@@ -25,6 +28,7 @@ app.get('/', (req,res) => {
 
 // defining the routes for the app
 app.use('/api/course', courseRoute)  // course route
+app.use('/api/user', userRoute)  // user route
 
 // start the server
 app.listen(PORT, () => console.log(`Server is Running at PORT: ${PORT}`))
