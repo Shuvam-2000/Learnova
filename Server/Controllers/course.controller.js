d9cbaef51a978af7addc9ef01592fd3e81ad5b79
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-06-03 17:31:27 +0530

    feat: image uplaod functionality done for courses

--- Server/Controllers/course.controller.js
+++ Server/Controllers/course.controller.js
@@ -1,16 +1,37 @@
 import CourseModel from "../models/courses.model.js";
+import { v2 as cloudinary } from 'cloudinary'
 
 // creating new courses
 export const createNewCourse = async (req,res) => {
     try {
-        const { courseName, courseDescription, coursePrice, image } = req.body
+        const { courseName, courseDescription, coursePrice } = req.body
 
         // extract userId from middleware
         const userId = req.user?.id
 
-        // check if all fields are provided
+         // check if all fields are provided
         if( !courseName || !courseDescription || !coursePrice ) return res.status(400).json({
             message: "All Fields Required",
+            success: false
+        })
+
+        // image upload
+        const { image } = req.files
+        if(!req.files || Object.keys(req.files).length === 0) return res.status(400).json({
+            message: 'No Image Uploaded',
+        })
+
+        // check the user has uploaded image with right format
+        const allowedFormat = ["image/png", "image/jpeg"]
+        if(!allowedFormat.includes(image.mimetype)) return res.status(400).json({
+            message: 'Invalid File Format',
+        })
+
+        // cloudinary setup
+        const cloud_response = await cloudinary.uploader.upload(image.tempFilePath);
+
+        if(!cloud_response || cloud_response.error) return res.status(400).json({
+            message: 'Error Uploading Image',
             success: false
         })
         
@@ -26,7 +47,10 @@ export const createNewCourse = async (req,res) => {
             courseName,
             courseDescription,
             coursePrice,
-            image,
+            image:{
+                public_id: cloud_response.public_id,
+                url: cloud_response.secure_url
+            },
             coursecreated_by: userId
         })
 
